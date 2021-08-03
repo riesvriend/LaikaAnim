40ef4a95373518217e4eea2cc01fb4825523c1ee
Ries Vriend <ries@vriend.com>
2021-08-03 10:43:06 +0200

    AR Camera mover by ALT and arrows

--- Assets/Scripts/Synchrony/ARCameraMover.cs
+++ Assets/Scripts/Synchrony/ARCameraMover.cs
@@ -9,10 +9,14 @@ using static Synchrony.SynchronyUtils;
 
 /// <summary>
 /// Allows moving the AR Camera while in Play mode in the Editor
+/// Mouse: 
+///  --left button down & move == move camera left right (X) and up down (Y)
+///  --scroll is move forward & backward (Z)
 /// </summary>
 public class ARCameraMover : MonoBehaviour
 {
-    [SerializeField] float mouseScrollScale = 5f;
+    [SerializeField] float mouseScrollMetersPerUpdate = 0.5f;
+    [SerializeField] float arrowKeyRotationDegreesPerSecond = 10f;
     [SerializeField] float mouseMoveScale = 5f;
 
     private Camera arCamera;
@@ -35,6 +39,11 @@ public class ARCameraMover : MonoBehaviour
 
     /// <summary>
     /// Rotate the camera with the mouse plus ALT or OPTION key 
+    /// Its not super intuitive, it needs a GUI rotation control similar to Unity Editor
+    /// Similar to an aircraft, we want the camera to:
+    /// - Right wing up/down to rotate X around Y (left/right)
+    /// - Tail vertical left/right to rotate Y around (control left/right)
+    /// - Tail wing up/down to rotate Z around X (up/down)
     /// </summary>
     private void RotateCamera()
     {
@@ -42,18 +51,30 @@ public class ARCameraMover : MonoBehaviour
         if (!allowRotate)
             return;
 
-        var xRotationDelta = Input.mouseScrollDelta.y;
-        var yRotationDelta = Input.GetAxis("Mouse X");
-        var zRotationDelta = Input.GetAxis("Mouse Y");
+        var leftRight = Input.GetKey(KeyCode.LeftArrow) ? 1 : Input.GetKey(KeyCode.RightArrow) ? -1 : 0;
+        var xRotationDirection = 0;
+        var yRotationDirection = 0;
+        var zRotationDirection = 0;
 
-        arCamera.transform.Rotate(RotationDegrees(xRotationDelta), RotationDegrees(yRotationDelta), RotationDegrees(zRotationDelta));
-
-        float RotationDegrees(float rotationDelta)
+        if (Input.GetKey(KeyCode.LeftControl))
+            zRotationDirection = leftRight;
+        else
         {
-            return rotationDelta * mouseMoveScale;
+            yRotationDirection = leftRight;
+
+            if (Input.GetKey(KeyCode.UpArrow))
+                xRotationDirection = 1;
+            else if (Input.GetKey(KeyCode.DownArrow))
+                xRotationDirection = -1;
+        }
+
+        arCamera.transform.Rotate(RotationAngle(xRotationDirection), RotationAngle(yRotationDirection), RotationAngle(zRotationDirection));
+
+        float RotationAngle(float rotationDelta)
+        {
+            return rotationDelta * arrowKeyRotationDegreesPerSecond * Time.deltaTime;
         }
     }
-    
 
     // Move left/right & up/down by moving mouse (with mouse down, no ALT key)
     private void MoveCamera()
@@ -65,8 +86,6 @@ public class ARCameraMover : MonoBehaviour
         // Left/Right and Up/Down
         var leftRightDelta = Input.GetAxis("Mouse X");
         var upDownDelta = Input.GetAxis("Mouse Y");
-
-        $"leftRightDelta: {leftRightDelta}".Log();
 
         arCamera.transform.position += new Vector3(x: ScaledMouseDelta(leftRightDelta), y: ScaledMouseDelta(upDownDelta), z: 0);
 
@@ -80,7 +99,8 @@ public class ARCameraMover : MonoBehaviour
         var forwardDelta = mouseScrollDeltaVector.y;
         if (forwardDelta != 0)
         {
-            var zOffset = mouseScrollScale * forwardDelta;
+            var zOffset = mouseScrollMetersPerUpdate * forwardDelta;
+            $"mouseScrollMetersPerUpdate: {mouseScrollMetersPerUpdate} * forwardDelta: {forwardDelta} = zOffset: {zOffset}".Log();
             arCamera.transform.position += new Vector3(x: 0, y: 0, z: zOffset);
         }
     }
