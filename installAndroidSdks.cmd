918f11f190af08bf64c2a926b7698cdcaf90df20
Ries Vriend <ries@vriend.com>
2021-08-30 10:40:23 +0200

    android release

---
+++ installAndroidSdks.cmd
@@ -0,0 +1,12 @@
+set UNITY_VERSION=2020.3.15f2
+ 
+c:
+set JAVA_HOME=c:\Unity\%UNITY_VERSION%\Editor\Data\PlaybackEngines\AndroidPlayer\OpenJDK\
+set ANDROID_HOME=c:\Unity\%UNITY_VERSION%\Editor\Data\PlaybackEngines\AndroidPlayer\
+cd %ANDROID_HOME%SDK\tools\bin\
+echo.> %USERPROFILE%\.android\repositories.cfg
+ 
+cmd /C sdkmanager --update
+cmd /C sdkmanager "platform-tools" "platforms;android-29"
+cmd /C sdkmanager "platform-tools" "platforms;android-30"
+cmd /C sdkmanager "platform-tools" "platforms;android-31"
