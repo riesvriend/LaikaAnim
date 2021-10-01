3de999dc2ae9230409e46613376d2f002c54f217
Ries <ries@vriend.com>
2021-10-01 17:59:31 +0200

    ios fixes

--- Assets/Plugins/iOS/SpeechRecorderViewController.mm
+++ Assets/Plugins/iOS/SpeechRecorderViewController.mm
@@ -92,9 +92,9 @@
             if (result) {
                 NSString *transcriptText = result.bestTranscription.formattedString;
                 NSLog(@"STARTRECORDING RESULT: %@", transcriptText);
-                //if (result.isFinal) {
+                if (transcriptText != nil) { //result.isFinal) {
                     UnitySendMessage("SpeechToText", "onResults", [transcriptText UTF8String]);
-                //}
+                }
             }
             else {
                 [audioEngine stop];
