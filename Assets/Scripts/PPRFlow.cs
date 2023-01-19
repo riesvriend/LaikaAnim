e03335a7820d37ede6df128aa29ed8fd5a38cd8f
Ries Vriend <ries@vriend.com>
2023-01-19 18:47:41 +0100

    states

---
+++ Assets/Scripts/PPRFlow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Power Pets Rescue gameflow / workflow
+/// </summary>
+[AddComponentMenu("PPR/Flow/PPR Flow"), DisallowMultipleComponent]
+public class PPRFlow : MonoBehaviour
+{
+    public List<PPRStartTransition> StartTransitions;
+}
---
+++ Assets/Scripts/PPRTransition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[AddComponentMenu("PPR/Flow/PPR Transition"), DisallowMultipleComponent]
+public class PPRTransition : MonoBehaviour
+{
+    public List<AnimalDef> AnimalsToAdd;
+    public PPRState PreviousState;
+    public PPRState NextState;
+
+    [UnityEngine.Header("Conditions")]
+    public bool IsTimeout;
+}
